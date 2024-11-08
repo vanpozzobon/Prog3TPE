e16231cf71ab6082261d3b81072ba5700a6c8d96
Vanina Pozzobon <vanpozzobon@gmail.com>
2024-11-08 11:53:57 -0300

    Quitar main

--- Arbol.java
+++ Arbol.java
@@ -21,18 +21,20 @@ public class Arbol {
 // aca empezar con los cambios a Tareas y ver si los ordenamos por que valor
 
     private TreeNode addRecursivo(Tarea t, TreeNode actual) {
-        if (actual == null)
+        if (actual == null) {
             return new TreeNode(t);
+        }
         if (actual.getPrioridad() == t.getPrioridad()) {
             actual.addTareaNodo(t);
             return actual;
         }
-        if (actual.getPrioridad() > t.getPrioridad())
-            actual.setIzq(this.addRecursivo(t,actual.getIzq()));
-        else
-            actual.setDer(this.addRecursivo(t,actual.getDer()));
-    return actual;
-      /*  if (actual.getTarea().getPrioridad() > t.getPrioridad()) {
+        if (actual.getPrioridad() > t.getPrioridad()) {
+            actual.setIzq(this.addRecursivo(t, actual.getIzq())); 
+        }else {
+            actual.setDer(this.addRecursivo(t, actual.getDer()));
+        }
+        return actual;
+        /*  if (actual.getTarea().getPrioridad() > t.getPrioridad()) {
             if (actual.getIzq() == null) {
                 actual.setIzq(new TreeNode(t));
             } else {
@@ -45,7 +47,7 @@ public class Arbol {
                 this.addRecursivo(t, actual.getDer());
             }
         }
-*/
+         */
     }
 
     public List<Tarea> obtenerTareasEnRango(int a, int b) {
@@ -74,8 +76,4 @@ public class Arbol {
 
     }
 
-    public static void main(String[] args) {
-        Arbol arbol = new Arbol();
-
-    }
 }
